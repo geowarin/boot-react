1b35e7c9f0e87d49e102ff23f1d67c8cfad0d1b6
geowarin <geowarin@gmail.com>
2015-08-05 17:54:35 +0200

    Fetch json from the boot server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    fetch('/api/simple')
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        console.log(res)
+      });
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello, guys.</h1>
     );
   }
 }
