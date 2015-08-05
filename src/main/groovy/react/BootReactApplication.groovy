c290269a9e105688b38dcc6cc0b3293ec85199e6
geowarin <geowarin@gmail.com>
2015-08-05 17:36:16 +0200

    Create the backend application with start.spring.io
    I used the following command to generate the app:
    curl https://start.spring.io/starter.tgz \
    -d name=boot-react  \
    -d bootVersion=1.3.0.M2 \
    -d dependencies=devtools,web \
    -d language=groovy \
    -d JavaVersion=1.8 \
    -d type=gradle-project \
    -d packageName=react \
    -d packaging=jar \
    -d artifactId=boot-react \
    -d baseDir=boot-react | tar -xzvf -
    
    I also added the .gitignore file

---
+++ src/main/groovy/react/BootReactApplication.groovy
@@ -0,0 +1,12 @@
+package react
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BootReactApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run BootReactApplication, args
+    }
+}
