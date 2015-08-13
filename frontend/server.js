2eb6460812c2f2022b49e04f8ed4034dda402c2c
geowarin <geowarin@gmail.com>
2015-08-13 15:03:05 +0200

    Simpler proxy

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,10 @@ var config = require('./webpack.dev.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    "*": "http://localhost:8080"
+  }
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
