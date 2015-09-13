6d73323e5f698059a607d59022f369a533e74834
geowarin <geowarin@gmail.com>
2015-09-13 23:08:00 +0200

    Fix session configuration

--- backend/src/main/groovy/react/config/redis/EmbeddedSessionConfig.groovy
+++ backend/src/main/groovy/react/config/redis/EmbeddedSessionConfig.groovy
@@ -1,12 +1,14 @@
 package react.config.redis
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.session.MapSessionRepository
 import org.springframework.session.web.http.HeaderHttpSessionStrategy
 import org.springframework.session.web.http.SessionRepositoryFilter
 
 @Profile('fake-redis')
+@Configuration
 public class EmbeddedSessionConfig {
 
   @Bean
--- backend/src/main/groovy/react/config/redis/ExternalRedisConfig.groovy
+++ backend/src/main/groovy/react/config/redis/ExternalRedisConfig.groovy
@@ -1,12 +1,14 @@
 package react.config.redis
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import org.springframework.session.web.http.HeaderHttpSessionStrategy
 
 @Profile('redis')
 @EnableRedisHttpSession
+@Configuration
 public class ExternalRedisConfig {
   @Bean
   HeaderHttpSessionStrategy sessionStrategy() {
