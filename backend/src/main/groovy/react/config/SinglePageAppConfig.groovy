2ea59121c5b6b5092e43896a949e70db60e8345d
geowarin <geowarin@gmail.com>
2015-09-29 12:02:20 +0200

    Improve PushStateResolver

--- backend/src/main/groovy/react/config/SinglePageAppConfig.groovy
+++ backend/src/main/groovy/react/config/SinglePageAppConfig.groovy
@@ -1,9 +1,11 @@
 package react.config
 
+import groovy.transform.builder.Builder
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
+import org.springframework.util.StringUtils
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.resource.ResourceResolver
@@ -24,24 +26,42 @@ class SinglePageAppConfig extends WebMvcConfigurerAdapter {
     registry.addResourceHandler('/**')
       .addResourceLocations('classpath:/static/')
       .resourceChain(false)
-      .addResolver(new SpaResourceResolver())
+      .addResolver(new PushStateResourceResolver())
   }
 
-  class SpaResourceResolver implements ResourceResolver {
+  @Builder
+  class PushStateResourceResolver implements ResourceResolver {
     Resource index = new ClassPathResource('/static/index.html')
+    String[] handledExtensions = ['html', 'js', 'json', 'csv', 'css', 'png', 'svg', 'eot', 'ttf', 'woff', 'appcache', 'jpg', 'jpeg', 'gif', 'ico']
+    String[] ignoredPaths = ['api']
 
     @Override
     Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
-      if (requestPath.startsWith('api')) {
-        return null
-      }
-      def foundResource = locations.collect { it.createRelative(requestPath) }.find { it.exists() }
-      return foundResource ?: index
+      resolve(requestPath, locations)
     }
 
     @Override
     String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-      'index'
+      resolve(resourcePath, locations)?.URL
+    }
+
+    private Resource resolve(String requestPath, List<? extends Resource> locations) {
+      if (isIgnored(requestPath)) {
+        return null
+      }
+      if (isHandled(requestPath)) {
+        return locations.collect { it.createRelative(requestPath) }.find { it.exists() }
+      }
+      return index
+    }
+
+    boolean isIgnored(String path) {
+      ignoredPaths.contains(path)
+    }
+
+    boolean isHandled(String path) {
+      def extension = StringUtils.getFilenameExtension(path)
+      handledExtensions.find { it == extension }
     }
   }
 }
