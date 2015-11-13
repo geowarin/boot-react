0fef86c77e00844d45fcf0b7df145735d50ee30d
geowarin <geowarin@gmail.com>
2015-11-13 16:57:16 +0100

    Make Component a stateless functional component

--- frontend/src/ui/Component.js
+++ frontend/src/ui/Component.js
@@ -3,33 +3,22 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import fetchResource from 'actions/fetchResource';
 
-export class MyComponent extends Component {
-  static propTypes = {
-    fetchResource: React.PropTypes.func.isRequired,
-    items: React.PropTypes.array.isRequired
-  };
+const MyComponent = (props) => {
+  var items = props.items;
+  var list = items.map((item, index) => <li key={index}>{item}</li>);
 
-  fetch() {
-    this.props.fetchResource();
-  }
-
-  render() {
-    var items = this.props.items;
-    var list = items.map((item, index) => <li key={index}>{item}</li>);
-
-    return (
-      <div>
-        <h2>Data:</h2>
-        <ul>
-          {list}
-        </ul>
-        <button onClick={this.fetch.bind(this)}>
-          Fetch
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>Data:</h2>
+      <ul>
+        {list}
+      </ul>
+      <button onClick={props.fetchResource}>
+        Fetch
+      </button>
+    </div>
+  );
+};
 
 export default connect(
     state => ({items: state.simple.items}),
