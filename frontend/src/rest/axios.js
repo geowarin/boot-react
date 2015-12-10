4763437795920d6cb58581c78d57cfa505e5a1d9
geowarin <geowarin@gmail.com>
2015-12-10 14:32:43 +0100

    Update the README

--- frontend/src/rest/axios.js
+++ frontend/src/rest/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { displayAuthError } from 'reducers/authentication';
 import { pushPath } from 'redux-simple-router';
 
-const setupAxiosInterceptors = (dispatch) => {
+const setupAxiosInterceptors = dispatch => {
   const onRequestSuccess = config => {
     var token = localStorage.getItem('auth-token');
     if (token) {
@@ -10,8 +10,8 @@ const setupAxiosInterceptors = (dispatch) => {
     }
     return config;
   };
-  const onResponseSuccess = (response) => response;
-  const onResponseError = (error) => {
+  const onResponseSuccess = response => response;
+  const onResponseError = error => {
     if (error.status == 403 && error.config.url != '/api/session') {
       const currentPath = window.location.pathname;
       dispatch(displayAuthError('Please login to access this resource'));
